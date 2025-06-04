f3079b85925b83ecff902ac6debd4b859d2c5729
jeya-prashanth <jeyaprashanth17@gmail.com>
2025-06-04 23:52:56 +0530

    add navbar and sidebar files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import SideBar from './components/SideBar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './contexts/Playercontext'
 
 const App = () => {
+
+  const {audioRef, track} = useContext(PlayerContext)
+
   return (
-    <div>
-      
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <SideBar/>
+        <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
